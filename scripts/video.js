f52bf240d9b30f4d29679b4c562381d7fdce433d
ahsankabirr <mdahsankabirrazu@gmail.com>
2026-03-25 12:29:11 +0200

    thumbnail section ad

--- scripts/video.js
+++ scripts/video.js
@@ -5,18 +5,12 @@ const loadCategories = () => {
     .catch((err) => console.error(err));
 };
 
-
 //     "category_id": "1001",
 //     "category": "Music"
 
-
 const displayCategories = (categories) => {
-  console.log(categories);
-
   const categoryContainer = document.getElementById("categories");
   categories.forEach((item) => {
-    console.log(item);
-
     // Button create
     const button = document.createElement("button");
     button.classList = "btn";
@@ -27,4 +21,76 @@ const displayCategories = (categories) => {
   });
 };
 
+// Load Video
+const loadVideo = () => {
+  fetch("https://openapi.programming-hero.com/api/phero-tube/videos")
+    .then((res) => res.json())
+    .then((data) => displayVideo(data.videos))
+    .catch((err) => console.error(err));
+};
+
+const cardDemo = {
+  category_id: "1001",
+  video_id: "aaaa",
+  thumbnail: "https://i.ibb.co/L1b6xSq/shape.jpg",
+  title: "Shape of You",
+  authors: [
+    {
+      profile_picture: "https://i.ibb.co/D9wWRM6/olivia.jpg",
+      profile_name: "Olivia Mitchell",
+      verified: "",
+    },
+  ],
+  others: {
+    views: "100K",
+    posted_date: "16278",
+  },
+  description:
+    "Dive into the rhythm of 'Shape of You,' a captivating track that blends pop sensibilities with vibrant beats. Created by Olivia Mitchell, this song has already gained 100K views since its release. With its infectious melody and heartfelt lyrics, 'Shape of You' is perfect for fans looking for an uplifting musical experience. Let the music take over as Olivia's vocal prowess and unique style create a memorable listening journey.",
+};
+
+const displayVideo = (videos) => {
+  const videoContainer = document.getElementById("videos");
+  videos.forEach((video) => {
+    // console.log(video);
+    const card = document.createElement("div");
+    card.classList = "card bg-base-100 ";
+    card.innerHTML = `
+  <figure class="h-[200px] relative">
+    <img
+      class="h-full w-full object-cover"
+      src="${video.thumbnail}"
+      alt="Shoes" />
+
+      ${video.others.posted_date?.length === 0 ? `` : `<span class="absolute right-8 bottom-8 bg-black text-white rounded p-1 ">${video.others.posted_date}</span>`}
+      
+  </figure>
+  <div class="px-0 py-2 flex gap-2">
+    <div>
+    <img
+      class="w-10 h-10 rounded-full object-cover"
+      src="${video.authors[0].profile_picture}"
+      alt="profile picture" />
+    </div>
+    <div>
+    <h2 class="font-bold">${video.title}</h2>
+    <div class="flex">
+      <p class="pr-2 text-gray-400 font-semibold">${video.authors[0].profile_name}</p>
+      
+      ${video.authors[0].verified === true ? `<img class="w-5 object-cover" src="https://img.icons8.com/?size=96&id=D9RtvkuOe31p&format=png" />` : ``}
+      
+      
+    </div>
+    <p class="text-gray-400">${video.others.views}</p>
+    
+    
+    </div>
+  </div>
+    `;
+
+    videoContainer.append(card);
+  });
+};
+
 loadCategories();
+loadVideo();
